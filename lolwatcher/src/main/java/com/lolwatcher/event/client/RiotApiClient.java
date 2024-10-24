de76b26893add8dd7edab28dd10053e42a78f93d
이창현 <changhyunlee96@gmail.com>
2024-10-24 16:44:36 +0900

    feat: getMatchAndTimeLineData method init

--- lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
@@ -2,6 +2,8 @@ package com.lolwatcher.event.client;
 
 import com.lolwatcher.event.config.FeignConfig;
 import com.lolwatcher.event.dto.AccountDto;
+import com.lolwatcher.event.dto.MatchData;
+import com.lolwatcher.event.dto.TimeLineData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,13 @@ public interface RiotApiClient {
     @RequestMapping(method = RequestMethod.GET, value = "/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
     AccountDto getSummonerRequest(@PathVariable String gameName, @PathVariable String tagLine);
 
-    @RequestMapping(method = RequestMethod.GET value = "/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20")
+    @RequestMapping(method = RequestMethod.GET, value = "/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20")
     List<String> getMatchIds(@PathVariable String puuid);
 
+    @RequestMapping(method = RequestMethod.GET, value = "/lol/match/v5/matches/{matchId}")
+    MatchData getMatchData(@PathVariable String matchId);
 
+    @RequestMapping(method = RequestMethod.GET, value = "/lol/match/v5/matches/{matchId}/timeline")
+    TimeLineData getTimeLineData(@PathVariable String matchId);
 
 }
