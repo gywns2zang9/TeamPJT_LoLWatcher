c85109692debb902b630bbdf7133a7f11fff3f2b
이창현 <changhyunlee96@gmail.com>
2024-10-24 16:42:14 +0900

    feat: getMatchIds

--- lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
@@ -6,10 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @FeignClient(name = "RiotApiClient", url = "${riot.lol.url}", configuration = FeignConfig.class)
 public interface RiotApiClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
-    AccountDto sendGetSummonerRequest(@PathVariable String gameName, @PathVariable String tagLine);
+    AccountDto getSummonerRequest(@PathVariable String gameName, @PathVariable String tagLine);
+
+    @RequestMapping(method = RequestMethod.GET value = "/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20")
+    List<String> getMatchIds(@PathVariable String puuid);
+
+
 
 }
