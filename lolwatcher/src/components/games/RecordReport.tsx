abb6a6f625af91fe957d8101113ce030f18fd5b7
gywns2zang9 <gywns2zang9@gmai.com>
2024-10-24 14:03:40 +0900

    feat: player-position 추가

--- lolwatcher/src/components/games/RecordReport.tsx
+++ lolwatcher/src/components/games/RecordReport.tsx
@@ -1,71 +1,77 @@
-import React from 'react';
-import './RecordReport.css';
+import React from "react";
+import "./RecordReport.css";
 
 export default function RecordReport() {
   return (
-    <div className='report-container'>
-      <div className='score-container'>
-        <div className='my-team-container'>97</div>
-        <div style={{ fontWeight: 'bold', fontSize: '20px' }}>:</div>
-        <div className='enemy-team-container'>97</div>
+    <div className="report-container">
+      <div className="score-container">
+        <div className="my-team-container">97</div>
+        <div style={{ fontWeight: "bold", fontSize: "20px" }}>:</div>
+        <div className="enemy-team-container">97</div>
       </div>
-      <div className='report-page'>
-        <div className='player-team'>
-          <div className='team-item'>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <div>탑</div>
-              <div className='champion-profile'></div>
-            </div>
+      <div className="report-page">
+        <div className="player-position">
+          <div className="position-line">탑</div>
+          <div className="position-line">정글</div>
+          <div className="position-line">미드</div>
+          <div className="position-line">원딜</div>
+          <div className="position-line">서폿</div>
+        </div>
+        <div className="rotated-line"></div>
+
+        <div className="player-team">
+          <div className="team-item">
+            <div className="champion-profile"></div>
             <div>로댕맨</div>
             <div>20</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
+          <div className="team-item">
+            <div className="champion-profile"></div>
             <div>로댕맨</div>
             <div>20</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
+          <div className="team-item">
+            <div className="champion-profile"></div>
             <div>로댕맨</div>
             <div>20</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
+          <div className="team-item">
+            <div className="champion-profile"></div>
             <div>로댕맨</div>
             <div>20</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
+          <div className="team-item">
+            <div className="champion-profile"></div>
             <div>로댕맨</div>
             <div>20</div>
           </div>
         </div>
-        <div className='rotated-line'></div>
-        <div className='enemy-team'>
-          <div className='team-item'>
+        <div className="rotated-line"></div>
+        <div className="enemy-team">
+          <div className="team-item">
             <div>20</div>
+            <div className="champion-profile"></div>
             <div>S2xSense</div>
-            <div className='champion-profile'></div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
-            <div>로댕맨</div>
+          <div className="team-item">
             <div>20</div>
+            <div className="champion-profile"></div>
+            <div>로댕맨</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
-            <div>로댕맨</div>
+          <div className="team-item">
             <div>20</div>
+            <div className="champion-profile"></div>
+            <div>로댕맨</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
-            <div>로댕맨</div>
+          <div className="team-item">
             <div>20</div>
+            <div className="champion-profile"></div>
+            <div>로댕맨</div>
           </div>
-          <div className='team-item'>
-            <div className='champion-profile'></div>
-            <div>로댕맨</div>
+          <div className="team-item">
             <div>20</div>
+            <div className="champion-profile"></div>
+            <div>로댕맨</div>
           </div>
         </div>
       </div>
