08c4e9aa46f71cecdb68a49bda6822eb03219553
이창현 <changhyunlee96@gmail.com>
2024-10-24 16:38:05 +0900

    feat: get summoner method

--- lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/client/RiotApiClient.java
@@ -1,10 +1,15 @@
 package com.lolwatcher.event.client;
 
 import com.lolwatcher.event.config.FeignConfig;
+import com.lolwatcher.event.dto.AccountDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "RiotApiClient", url = "${riot.lol.url}", configuration = FeignConfig.class)
 public interface RiotApiClient {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
+    AccountDto sendGetSummonerRequest(@PathVariable String gameName, @PathVariable String tagLine);
 
 }
--- lolwatcher/src/main/java/com/lolwatcher/event/config/FeignConfig.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/config/FeignConfig.java
@@ -2,6 +2,8 @@ package com.lolwatcher.event.config;
 
 import feign.Client;
 import feign.Feign;
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +11,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
 
+    @Value("${riot.lol.api-key}")
+    private String apiKey;
+
+
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
     }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            // API 키를 쿼리 파라미터로 추가
+            requestTemplate.query("api_key", apiKey);
+        };
+    }
+}
 
 
 }
