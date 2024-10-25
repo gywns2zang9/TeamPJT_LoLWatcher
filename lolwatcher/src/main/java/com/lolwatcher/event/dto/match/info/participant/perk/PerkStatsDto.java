10133c6eac0109ae0bff10ae6c2ab752cf022ee5
이창현 <changhyunlee96@gmail.com>
2024-10-25 18:03:00 +0900

    refactor: match data dto fix

---
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/match/info/participant/perk/PerkStatsDto.java
@@ -0,0 +1,6 @@
+package com.lolwatcher.event.dto.match.info.participant.perk;
+
+public record PerkStatsDto(int offense,
+                           int defense,
+                           int flex) {
+}
---
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/match/info/team/ObjectivesDto.java
@@ -0,0 +1,4 @@
+package com.lolwatcher.event.dto.match.info.team;
+
+public record ObjectivesDto(ObjectiveDto baron, ObjectiveDto champion, ObjectiveDto dragon, ObjectiveDto horde, ObjectiveDto inhibitor, ObjectiveDto riftHerald, ObjectiveDto tower) {
+}
