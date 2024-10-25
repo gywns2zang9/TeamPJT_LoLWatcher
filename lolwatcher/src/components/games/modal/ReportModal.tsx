9e8421b51803ca46982801b46ee261a2a75845d5
yun090405 <yun090405@naver.com>
2024-10-25 16:39:57 +0900

    fix: modal design

--- lolwatcher/src/components/games/modal/ReportModal.tsx
+++ lolwatcher/src/components/games/modal/ReportModal.tsx
@@ -4,33 +4,37 @@ import './ReportModal.css';
 export default function ReportModal() {
   return (
     <div className='modal-container'>
-      <h1>ReportModal</h1>
-      <div className='position-point'>
-        <div
-          style={{
-            width: '40px',
-            height: '40px',
-            borderRadius: '50%',
-            backgroundColor: 'gray',
-            textAlign: 'center',
-          }}
-        ></div>
-        <div style={{ backgroundColor: 'blue' }}>position</div>
-        <div>champion</div>
-        <div>first_important</div>
-        <div>second_important</div>
-        <div>third_important</div>
-        <div>fourth_important</div>
-        {/* <div>fifth_important</div>
-        <div>sixth_important</div> */}
+      <div style={{ width: '50%', height: '100%', flexDirection: 'column', textAlign: 'center' }}>
+        <div className='position-point'>
+          <div className='champion-profile-detail'></div>
+          <div style={{ fontWeight: 'bold', fontSize: '17px', color: 'gray', margin: 'auto', marginTop: '10px' }}>
+            피즈
+          </div>
+          <div style={{ fontWeight: 'bold', color: 'gray', fontSize: '20px', margin: 'auto' }}>탑 라이너</div>
+          <div style={{ marginTop: '10px', justifyContent: 'space-between', paddingInline: '20px' }}>
+            <div style={{ fontWeight: 'bold', fontSize: '20px' }}>솔로 킬</div>
+            <div style={{ fontWeight: 'bold', fontSize: '20px', color: 'gold' }}>20</div>
+          </div>
+          <div style={{ justifyContent: 'space-between', paddingInline: '20px' }}>
+            <div style={{ fontWeight: 'bold', fontSize: '20px' }}>오브젝트 참여</div>
+            <div style={{ fontWeight: 'bold', fontSize: '20px', color: 'gold' }}>20</div>
+          </div>
+          <div style={{ justifyContent: 'space-between', paddingInline: '20px' }}>
+            <div style={{ fontWeight: 'bold', fontSize: '20px' }}>가한 피해량</div>
+            <div style={{ fontWeight: 'bold', fontSize: '20px', color: 'gold' }}>20</div>
+          </div>
+          <div style={{ justifyContent: 'space-between', paddingInline: '20px' }}>
+            <div style={{ fontWeight: 'bold', fontSize: '20px' }}>암튼 점수</div>
+            <div style={{ fontWeight: 'bold', fontSize: '20px', color: 'red' }}>2</div>
+          </div>
+        </div>
+        <div className='common-point'>
+          <div style={{ fontWeight: 'bold', color: 'gray', fontSize: '20px' }}>공통 지표</div>
+          <div>성장</div>
+          <div>영향력</div>
+        </div>
       </div>
-      <div className='common-point'>
-        <div>kda</div>
-        <div>kda</div>
-        <div>kda</div>
-        <div>kda</div>
-        <div>kda</div>
-      </div>
+      <div style={{ width: '50%', height: '100%', backgroundColor: 'gray' }}></div>
     </div>
   );
 }
