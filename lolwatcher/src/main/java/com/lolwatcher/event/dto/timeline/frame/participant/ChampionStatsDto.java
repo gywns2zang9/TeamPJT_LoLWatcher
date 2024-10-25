5976b20faf278f72314640812ef47d4f35f1f97d
이창현 <changhyunlee96@gmail.com>
2024-10-25 14:56:12 +0900

    fix: dto shape fix

--- lolwatcher/src/main/java/com/lolwatcher/event/dto/timeline/frame/participant/ChampionStatsDto.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/timeline/frame/participant/ChampionStatsDto.java
@@ -1,4 +1,29 @@
 package com.lolwatcher.event.dto.timeline.frame.participant;
 
-public record ChampionStatsDto(int abilityHaste, int abilityPower, int armor, int armorPen, int armorPenPercent, int attackDamage, int attackSpeed, int bonusArmorPenPercent, int bonusMagicPenPercent, int ccReduction, int cooldownReduction, int health, int healthMax, int healthRegen, int lifesteal, int magicPen, int magicPenPercent, int magicResist, int movementSpeed, int omnivamp, int pysicalVamp, int power, int powerMax, int powerRegen, int spellVamp) {
+public record ChampionStatsDto(int abilityHaste,
+                               int abilityPower,
+                               int armor,
+                               int armorPen,
+                               int armorPenPercent,
+                               int attackDamage,
+                               int attackSpeed,
+                               int bonusArmorPenPercent,
+                               int bonusMagicPenPercent,
+                               int ccReduction,
+                               int cooldownReduction,
+                               int health,
+                               int healthMax,
+                               int healthRegen,
+                               int lifesteal,
+                               int magicPen,
+                               int magicPenPercent,
+                               int magicResist,
+                               int movementSpeed,
+                               int omnivamp,
+                               int pysicalVamp,
+                               int power,
+                               int powerMax,
+                               int powerRegen,
+                               int spellVamp) {
 }
+
--- lolwatcher/src/main/java/com/lolwatcher/event/dto/timeline/frame/participant/ParticipantFrameDto.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/timeline/frame/participant/ParticipantFrameDto.java
@@ -2,5 +2,16 @@ package com.lolwatcher.event.dto.timeline.frame.participant;
 
 import com.lolwatcher.event.dto.timeline.frame.common.PositionDto;
 
-public record ParticipantFrameDto(ChampionStatsDto championStats, int currentGold, DamageStatsDto damageStats, int goldPerSecond, int jungleMinionsKilled, int level, int minionsKilled, int participantId, PositionDto position, int timeEnemySpentControlled, int totalGold, int xp) {
+public record ParticipantFrameDto(ChampionStatsDto championStats,
+                                  int currentGold,
+                                  DamageStatsDto damageStats,
+                                  int goldPerSecond,
+                                  int jungleMinionsKilled,
+                                  int level,
+                                  int minionsKilled,
+                                  int participantId,
+                                  PositionDto position,
+                                  int timeEnemySpentControlled,
+                                  int totalGold,
+                                  int xp) {
 }
