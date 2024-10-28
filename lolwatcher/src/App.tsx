acab66ab832da3f1a44b790dcd717ea3b4010140
yun090405 <yun090405@naver.com>
2024-10-28 16:05:04 +0900

    feat: Regist.tsx Route in App.tsx

--- lolwatcher/src/App.tsx
+++ lolwatcher/src/App.tsx
@@ -1,17 +1,30 @@
-import "./App.css";
-import Main from "./pages/Main";
-import Login from "./pages/Login";
-import GameRecord from "./pages/GameRecord";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import GameRecord from './pages/GameRecord';
+import Regist from './pages/Regist';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/records" element={<GameRecord />} />
+        <Route
+          path='/'
+          element={<Main />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/records'
+          element={<GameRecord />}
+        />
+        <Route
+          path='/regist'
+          element={<Regist />}
+        />
       </Routes>
     </Router>
   );
