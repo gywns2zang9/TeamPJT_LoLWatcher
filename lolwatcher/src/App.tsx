3acacfb68c69be53638a2f36b3162fcaf2d44780
gywns2zang9 <gywns2zang9@gmai.com>
2024-10-29 12:41:37 +0900

    feat: 상세페이지 생성

--- lolwatcher/src/App.tsx
+++ lolwatcher/src/App.tsx
@@ -1,30 +1,20 @@
-import './App.css';
-import Main from './pages/Main';
-import Login from './pages/Login';
-import GameRecord from './pages/GameRecord';
-import Regist from './pages/Regist';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import GameRecord from "./pages/GameRecord";
+import Regist from "./pages/Regist";
+import ResultReport from "./pages/ResultReport";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path='/'
-          element={<Main />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/records'
-          element={<GameRecord />}
-        />
-        <Route
-          path='/regist'
-          element={<Regist />}
-        />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/records" element={<GameRecord />} />
+        <Route path="/regist" element={<Regist />} />
+        <Route path="/result" element={<ResultReport />} />
       </Routes>
     </Router>
   );
---
+++ lolwatcher/src/pages/ResultReport.tsx
@@ -0,0 +1,11 @@
+import "./ResultReport.css";
+import GlobalGold from "../components/Result/GlobalGold";
+
+export default function ResultReport() {
+  return (
+    <div>
+      <h1>결과리포트</h1>
+      <GlobalGold />
+    </div>
+  );
+}
