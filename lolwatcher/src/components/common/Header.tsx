3721653c02aaa12f994b3ed34d9daf3069aa3aa9
gywns2zang9 <gywns2zang9@gmai.com>
2024-10-30 13:46:00 +0900

    design: css 수정

--- lolwatcher/src/components/common/Header.tsx
+++ lolwatcher/src/components/common/Header.tsx
@@ -5,7 +5,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-container">
-      <h1>Header</h1>
+      <h1>소환사 검색</h1>
       <SearchBar />
     </div>
   );
--- lolwatcher/src/pages/Main.tsx
+++ lolwatcher/src/pages/Main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Header from "../components/common/Header";
+import SearchBar from "../components/common/SearchBar";
 import "./Main.css";
 
 export default function Main() {
@@ -12,10 +12,10 @@ export default function Main() {
 
   return (
     <div className="main-container">
-      <Header />
       <div className="main-content">
         <h1 className="main-intro">LoL Watcher</h1>
       </div>
+      <SearchBar />
       <button className="main-btn" onClick={handleLogin}>
         서비스 이용하기
       </button>
