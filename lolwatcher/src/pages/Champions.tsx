d600827f3250986c22dfc229127a7d008ba436f6
gywns2zang9 <gywns2zang9@gmai.com>
2024-11-01 16:39:21 +0900

    feat: champions 페이지 생성

---
+++ lolwatcher/src/pages/Champions.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Champions.css";
+
+export default function Champions() {
+  const CHAMPION_IMG_BASE_URL =
+    "https://ddragon.leagueoflegends.com/cdn/14.21.1/img/champion/";
+
+    const champions = [
+      "Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", 
+      "AurelionSol", "Azir", "Bard", "Belveth", "Blitzcrank", "Brand", "Braum", "Caitlyn", 
+      "Camille", "Cassiopeia", "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", 
+      "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", 
+      "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Hwei", "Illaoi", "Irelia", 
+      "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "KSante", "Kaisa", "Kalista", 
+      "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Khazix", "Kindred", 
+      "Kled", "KogMaw", "Leblanc", "LeeSin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", 
+      "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "Milio", "MissFortune", "MonkeyKing","Mordekaiser", 
+      "Morgana", "Naafiri","Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu", 
+      "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", 
+      "RekSai", "Rell", "Renata", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", 
+      "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", 
+      "Sivir", "Skarner", "Smolder", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "TahmKench", "Taliyah", 
+      "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", 
+      "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vex", "Vi", "Viego", 
+      "Viktor", "Vladimir", "Volibear", "Warwick", "Xayah", "Xerath", "XinZhao", 
+      "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", 
+      "Zyra"
+    ];
+    
+
+  return (
+    <div className="champions-container">
+      <div className="champion-list">
+        {champions.map((champion, index) => (
+          <div key={index} className="champion-item">
+            <img
+              src={`${CHAMPION_IMG_BASE_URL}${champion}.png`}
+              alt={champion}
+            />
+            <p>{champion}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
