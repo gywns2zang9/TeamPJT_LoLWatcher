6d745f50d8a3f97993421b4843dfac9a960c7ca9
gywns2zang9 <gywns2zang9@gmai.com>
2024-11-04 19:04:39 +0900

    feat: 챔피언 정보 모달 추가

--- lolwatcher/src/pages/Champions.tsx
+++ lolwatcher/src/pages/Champions.tsx
@@ -1,47 +1,209 @@
-import React from "react";
+import React, { useState } from "react";
+import InfoModal from "../components/champions/InfoModal";
 import "./Champions.css";
 
 export default function Champions() {
   const CHAMPION_IMG_BASE_URL =
     "https://ddragon.leagueoflegends.com/cdn/14.21.1/img/champion/";
 
-    const champions = [
-      "Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", 
-      "AurelionSol", "Azir", "Bard", "Belveth", "Blitzcrank", "Brand", "Braum", "Caitlyn", 
-      "Camille", "Cassiopeia", "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", 
-      "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", 
-      "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Hwei", "Illaoi", "Irelia", 
-      "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "KSante", "Kaisa", "Kalista", 
-      "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Khazix", "Kindred", 
-      "Kled", "KogMaw", "Leblanc", "LeeSin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", 
-      "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "Milio", "MissFortune", "MonkeyKing","Mordekaiser", 
-      "Morgana", "Naafiri","Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu", 
-      "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", 
-      "RekSai", "Rell", "Renata", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", 
-      "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", 
-      "Sivir", "Skarner", "Smolder", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "TahmKench", "Taliyah", 
-      "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", 
-      "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vex", "Vi", "Viego", 
-      "Viktor", "Vladimir", "Volibear", "Warwick", "Xayah", "Xerath", "XinZhao", 
-      "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", 
-      "Zyra"
-    ];
-    
+  const champions = [
+    "Aatrox",
+    "Ahri",
+    "Akali",
+    "Alistar",
+    "Amumu",
+    "Anivia",
+    "Annie",
+    "Aphelios",
+    "Ashe",
+    "AurelionSol",
+    "Azir",
+    "Bard",
+    "Belveth",
+    "Blitzcrank",
+    "Brand",
+    "Braum",
+    "Caitlyn",
+    "Camille",
+    "Cassiopeia",
+    "Chogath",
+    "Corki",
+    "Darius",
+    "Diana",
+    "DrMundo",
+    "Draven",
+    "Ekko",
+    "Elise",
+    "Evelynn",
+    "Ezreal",
+    "Fiddlesticks",
+    "Fiora",
+    "Fizz",
+    "Galio",
+    "Gangplank",
+    "Garen",
+    "Gnar",
+    "Gragas",
+    "Graves",
+    "Gwen",
+    "Hecarim",
+    "Heimerdinger",
+    "Hwei",
+    "Illaoi",
+    "Irelia",
+    "Ivern",
+    "Janna",
+    "JarvanIV",
+    "Jax",
+    "Jayce",
+    "Jhin",
+    "Jinx",
+    "KSante",
+    "Kaisa",
+    "Kalista",
+    "Karma",
+    "Karthus",
+    "Kassadin",
+    "Katarina",
+    "Kayle",
+    "Kayn",
+    "Kennen",
+    "Khazix",
+    "Kindred",
+    "Kled",
+    "KogMaw",
+    "Leblanc",
+    "LeeSin",
+    "Leona",
+    "Lillia",
+    "Lissandra",
+    "Lucian",
+    "Lulu",
+    "Lux",
+    "Malphite",
+    "Malzahar",
+    "Maokai",
+    "MasterYi",
+    "Milio",
+    "MissFortune",
+    "MonkeyKing",
+    "Mordekaiser",
+    "Morgana",
+    "Naafiri",
+    "Nami",
+    "Nasus",
+    "Nautilus",
+    "Neeko",
+    "Nidalee",
+    "Nilah",
+    "Nocturne",
+    "Nunu",
+    "Olaf",
+    "Orianna",
+    "Ornn",
+    "Pantheon",
+    "Poppy",
+    "Pyke",
+    "Qiyana",
+    "Quinn",
+    "Rakan",
+    "Rammus",
+    "RekSai",
+    "Rell",
+    "Renata",
+    "Renekton",
+    "Rengar",
+    "Riven",
+    "Rumble",
+    "Ryze",
+    "Samira",
+    "Sejuani",
+    "Senna",
+    "Seraphine",
+    "Sett",
+    "Shaco",
+    "Shen",
+    "Shyvana",
+    "Singed",
+    "Sion",
+    "Sivir",
+    "Skarner",
+    "Smolder",
+    "Sona",
+    "Soraka",
+    "Swain",
+    "Sylas",
+    "Syndra",
+    "TahmKench",
+    "Taliyah",
+    "Talon",
+    "Taric",
+    "Teemo",
+    "Thresh",
+    "Tristana",
+    "Trundle",
+    "Tryndamere",
+    "TwistedFate",
+    "Twitch",
+    "Udyr",
+    "Urgot",
+    "Varus",
+    "Vayne",
+    "Veigar",
+    "Velkoz",
+    "Vex",
+    "Vi",
+    "Viego",
+    "Viktor",
+    "Vladimir",
+    "Volibear",
+    "Warwick",
+    "Xayah",
+    "Xerath",
+    "XinZhao",
+    "Yasuo",
+    "Yone",
+    "Yorick",
+    "Yuumi",
+    "Zac",
+    "Zed",
+    "Zeri",
+    "Ziggs",
+    "Zilean",
+    "Zoe",
+    "Zyra"
+  ];
+
+  const [selectedChampion, setSelectedChampion] = useState<string | null>(null);
+
+  const handleChampionClick = (champion: string) => {
+    setSelectedChampion(champion);
+  };
+
+  const closeModal = () => {
+    setSelectedChampion(null);
+  };
 
   return (
     <div className="champions-container">
       <div className="champion-list">
         {champions.map((champion, index) => (
-          <div key={index} className="champion-item">
+          <div
+            key={index}
+            className="champion-item"
+            onClick={() => handleChampionClick(champion)}
+          >
             <img
               src={`${CHAMPION_IMG_BASE_URL}${champion}.png`}
               alt={champion}
-              loading="lazy" // 지연 로딩
             />
             <p>{champion}</p>
           </div>
         ))}
       </div>
+      {selectedChampion && (
+        <InfoModal championName={selectedChampion} onClose={closeModal} />
+      )}
     </div>
   );
 }
--- lolwatcher/src/pages/Main.tsx
+++ lolwatcher/src/pages/Main.tsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import SearchBar from '../components/common/SearchBar';
-import './Main.css';
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import SearchBar from "../components/common/SearchBar";
+import "./Main.css";
 
 export default function Main() {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    const accessToken = localStorage.getItem('accessToken'); // sessionStorage 사용
+    const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
-      navigate('/record');
+      navigate("/record");
     } else {
-      navigate('/login');
+      navigate("/login");
     }
   };
 
   return (
-    <div className='main-container'>
-      <div className='main-content'>
-        <h1 className='main-intro'>LoL Watcher.com</h1>
+    <div className="main-container">
+      <div className="main-content">
+        <h1 className="main-intro">LoL Watcher.com</h1>
       </div>
       <SearchBar />
-      <button
-        className='main-btn'
-        onClick={handleLogin}
-      >
+      <button className="main-btn" onClick={handleLogin}>
         서비스 이용하기
       </button>
+      <NavLink to="/champions">챔피언 정보</NavLink>
     </div>
   );
 }
