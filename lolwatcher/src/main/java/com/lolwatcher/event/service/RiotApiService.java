6b7652a4ee985670b9677632f2447ca0ce3a026e
이창현 <changhyunlee96@gmail.com>
2024-11-04 17:37:34 +0900

    feat: get RiotInfo

--- lolwatcher/src/main/java/com/lolwatcher/event/service/RiotApiService.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/service/RiotApiService.java
@@ -4,6 +4,10 @@ import com.lolwatcher.event.client.PythonApiClient;
 import com.lolwatcher.event.client.RiotApiClient;
 import com.lolwatcher.event.dto.AccountDto;
 import com.lolwatcher.event.dto.match.MatchDto;
+import com.lolwatcher.event.dto.match.info.participant.ParticipantDto;
+import com.lolwatcher.event.dto.record.RecordDto;
+import com.lolwatcher.event.dto.record.RecordGameInfoDto;
+import com.lolwatcher.event.dto.record.RecordUserDto;
 import com.lolwatcher.event.dto.timeline.TimelineDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -40,12 +44,31 @@ public class RiotApiService {
     }
 
     public List<MatchDto> getMatchDataBySummoner(String name, String tag) {
-        List<String> matchIds = riotApiClient.getMatchIds(riotApiClient.getSummonerRequest(name, tag).puuid());
+        String puuid = riotApiClient.getSummonerRequest(name, tag).puuid();
+        List<String> matchIds = riotApiClient.getMatchIds(puuid);
         List<MatchDto> lists = new ArrayList<>();
         for(int i = 0 ; i < 5; i++) {
             lists.add(riotApiClient.getMatchData(matchIds.get(i)));
         }
+        List<RecordDto> recordList = matchDtoToRecordDto(puuid, lists);
         return lists;
+    }
+
+    private List<RecordDto> matchDtoToRecordDto(String puuid, List<MatchDto> lists) {
+        List<RecordDto> list = new ArrayList<>();
+        for(MatchDto matchDto : lists) {
+            List<RecordUserDto> users = new ArrayList<>();
+            boolean win = false;
+            for(ParticipantDto participant : matchDto.info().participants()) {
+                users.add(new RecordUserDto(participant.championName(), participant.summonerName(), participant.teamId(), participant.kills(), participant.assists(), participant.deaths(), participant.totalMinionsKilled()));
+                if(puuid.equals(participant.puuid())) {
+                    win = participant.win();
+                }
+            }
+            RecordGameInfoDto info = new RecordGameInfoDto(matchDto.info().gameEndTimestamp(), matchDto.info().gameDuration(), win);
+            list.add(new RecordDto(users, info));
+        }
+        return list;
     }
 
 }
