e541e59396dbda617a367d7b355daacfa829d03e
gywns2zang9 <gywns2zang9@gmai.com>
2024-11-05 10:35:38 +0900

    feat: skillvidoe 추가

--- lolwatcher/src/components/champions/InfoModal.tsx
+++ lolwatcher/src/components/champions/InfoModal.tsx
@@ -34,6 +34,7 @@ export default function InfoModal({
 
   const [spells, setSpells] = useState<Spell[]>([]);
   const [passiveImageUrl, setPassiveImageUrl] = useState<string | null>(null);
+  const [selectedVideoUrl, setSelectedVideoUrl] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchChampionData = async () => {
@@ -50,14 +51,27 @@ export default function InfoModal({
 
         setPassiveImageUrl(
           `${PASSIVE_IMG_BASE_URL}${championData.passive.image.full}`
-        );
+        ); // 패시브 스킬 비디오 URL 초기 설정
+        const paddedChampionKey = championKey.padStart(4, "0");
+        const initialVideoUrl = `https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${paddedChampionKey}/ability_${paddedChampionKey}_P1.webm`;
+        setSelectedVideoUrl(initialVideoUrl);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchChampionData();
-  }, [championId]);
+  }, [championId, championKey]);
+
+  // 클릭 시 비디오 URL 설정
+  const handleImageClick = (spellType: string) => {
+    // championKey를 4자리로 패딩
+    const paddedChampionKey = championKey.padStart(4, "0");
+
+    const videoUrl = `https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${paddedChampionKey}/ability_${paddedChampionKey}_${spellType}1.webm`;
+    console.log(videoUrl);
+    setSelectedVideoUrl(videoUrl);
+  };
 
   return (
     <div
@@ -72,23 +86,45 @@ export default function InfoModal({
         <p>{championBlurb}</p>
 
         <div className="spells-container">
+          {/* Passive 스킬 */}
           {passiveImageUrl && (
-            <div className="spell-item">
+            <div className="spell-item" onClick={() => handleImageClick("P")}>
               <img src={passiveImageUrl} alt="Passive" />
             </div>
           )}
-          {spells.map((spell) => (
-            <div key={spell.id} className="spell-item">
-              <img
-                src={`${SPELL_IMG_BASE_URL}${spell.id}.png`}
-                alt={spell.name}
-              />
-              {/* <h4>{spell.name}</h4> */}
-              {/* <p>{spell.description}</p> */}
-            </div>
-          ))}
+          {/* Q, W, E, R 스킬 */}
+          {spells.map((spell, index) => {
+            const spellType = ["Q", "W", "E", "R"][index];
+            return (
+              <div
+                key={spell.id}
+                className="spell-item"
+                onClick={() => handleImageClick(spellType)}
+              >
+                <img
+                  src={`${SPELL_IMG_BASE_URL}${spell.id}.png`}
+                  alt={spell.name}
+                />
+                {/* <h4>{spell.name}</h4> */}
+                {/* <p>{spell.description}</p> */}{" "}
+              </div>
+            );
+          })}
         </div>
 
+        {/* 비디오 모달 */}
+        {selectedVideoUrl && (
+          <div className="video-modal">
+            <div className="video-content">
+              <video
+                src={selectedVideoUrl}
+                autoPlay
+                controls
+                className="spell-video"
+              />
+            </div>
+          </div>
+        )}
         <button onClick={onClose}>닫기</button>
       </div>
     </div>
