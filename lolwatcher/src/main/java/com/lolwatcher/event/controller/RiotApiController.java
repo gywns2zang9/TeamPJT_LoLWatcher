3e7800fda9ec7377e4970d44c157448b49d8b1b4
이창현 <changhyunlee96@gmail.com>
2024-11-06 15:45:10 +0900

    fix: request mapping

--- lolwatcher/src/main/java/com/lolwatcher/event/controller/RiotApiController.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/controller/RiotApiController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/riot")
+@RequestMapping("/riot")
 @RequiredArgsConstructor
 @Slf4j
 public class RiotApiController {
