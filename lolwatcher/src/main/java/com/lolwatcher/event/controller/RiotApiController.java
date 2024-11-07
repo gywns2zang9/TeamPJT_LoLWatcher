9b1c7b0526e5984e2373e49086e40e4fb556da23
신규철 <kyuchul5243@gmail.com>
2024-11-07 15:45:58 +0900

    fix:merge fix

--- lolwatcher/src/main/java/com/lolwatcher/event/controller/RiotApiController.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/controller/RiotApiController.java
@@ -3,6 +3,7 @@ package com.lolwatcher.event.controller;
 import com.lolwatcher.event.dto.AccountDto;
 import com.lolwatcher.event.dto.match.MatchDto;
 import com.lolwatcher.event.dto.match.info.InfoDto;
+import com.lolwatcher.event.dto.record.RecordDto;
 import com.lolwatcher.event.dto.timeline.TimelineDto;
 import com.lolwatcher.event.service.RiotApiService;
 import lombok.RequiredArgsConstructor;
@@ -61,5 +62,6 @@ public class RiotApiController {
     public InfoDto getMatchPlayersInfo(@RequestParam("match") String matchId) {
         return riotApiService.getMatchPlayersInfo(matchId);
     }
+
 
 }
