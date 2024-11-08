d8133dc2f6c9aa5a6368c7c97046b4f557b9e62c
gywns2zang9 <gywns2zang9@gmai.com>
2024-11-08 15:10:15 +0900

    fix: 메인페이지에 로그인폼

--- lolwatcher/src/components/auth/LoginForm.tsx
+++ lolwatcher/src/components/auth/LoginForm.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { postLoginAPI } from "../../api/authApi";
+import { functionAccessToken, postLoginAPI } from "../../api/authApi";
 import "./LoginForm.css";
 
-const LoginForm: React.FC = () => {
+interface LoginFormProps {
+  toggleForm: () => void;
+}
+
+export default function LoginForm({ toggleForm }: LoginFormProps) {
+  const navigate = useNavigate();
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       console.log("로그인 시도");
       await postLoginAPI(userId, password);
-      console.log("로그인 성공");
+      window.alert("안녕하세요.");
       navigate("/users");
     } catch (err) {
       console.log("로그인 실패");
@@ -20,49 +24,36 @@ const LoginForm: React.FC = () => {
     }
   };
 
-  const handleRegist = () => {
-    clearInput();
-    navigate("/regist");
-  };
-
-  const clearInput = () => {
-    setUserId("");
-    setPassword("");
-  };
-
   return (
-    <div className="container">
-      <div className="insert-container">
-        <h2 className="logo">LoLWatcher</h2>
-        <div>
+    <div className="form-container">
+      <div className="form-main">
+        <div className="input-box">
           <input
-            className="insert"
+            className="input-field"
             type="text"
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
             placeholder="아이디"
+            maxLength={20}
           />
-        </div>
-        <div>
           <input
-            className="insert"
+            className="input-field"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="비밀번호"
+            maxLength={20}
           />
         </div>
-        <div style={{ display: "flex", justifyContent: "space-around" }}>
-          <button className="login-button" onClick={handleLogin}>
-            로그인
-          </button>
-          <button className="login-button" onClick={handleRegist}>
-            회원가입
-          </button>
-        </div>
+        <button className="login-button" onClick={handleLogin}>
+          로그인
+        </button>
       </div>
+
+      <p onClick={toggleForm} className="toggle-msg">
+        회원이 아니신가요?
+        <span>회원가입</span>
+      </p>
     </div>
   );
-};
-
-export default LoginForm;
+}
--- lolwatcher/src/pages/Main.tsx
+++ lolwatcher/src/pages/Main.tsx
@@ -1,30 +1,35 @@
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import SearchBar from "../components/common/SearchBar";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { functionAccessToken } from "../api/authApi";
+import LoginForm from "../components/auth/LoginForm";
+import RegistForm from "../components/auth/RegistForm";
 import "./Main.css";
 
 export default function Main() {
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      navigate("/record");
-    } else {
-      navigate("/login");
+  useEffect(() => {
+    const accesstoken = functionAccessToken();
+    if (!accesstoken) {
+      navigate("/users");
+      return;
     }
-  };
+  }, [navigate]);
+
+  const [showLogin, setShowLogin] = useState(true);
 
   return (
     <div className="main-container">
-      <div className="main-content">
-        <h1 className="main-intro">LoL Watcher.com</h1>
+      <div className="main-intro">
+        <h1 className="main-title">LoL Watcher .com</h1>
       </div>
-      <SearchBar />
-      <button className="main-btn" onClick={handleLogin}>
-        서비스 이용하기
-      </button>
-      <NavLink to="/champions">챔피언 정보</NavLink>
+      <div className="main-form-area">
+        {showLogin ? (
+          <LoginForm toggleForm={() => setShowLogin(false)} />
+        ) : (
+          // <RegistForm toggleForm={() => setShowLogin(true)} />
+          <RegistForm />
+        )}
+      </div>
     </div>
   );
 }
