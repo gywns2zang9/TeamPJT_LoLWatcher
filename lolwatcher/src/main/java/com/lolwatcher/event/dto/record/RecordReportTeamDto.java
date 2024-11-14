569fc0c007092915f2e2b9b37077fe883fc5f876
이창현 <changhyunlee96@gmail.com>
2024-11-14 09:57:50 +0900

    feat: record report dto init

--- lolwatcher/src/main/java/com/lolwatcher/event/dto/record/RecordReportTeamDto.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/record/RecordReportTeamDto.java
@@ -1,4 +1,13 @@
 package com.lolwatcher.event.dto.record;
 
-public record RecordReportTeamDto() {
+import java.util.List;
+import java.util.Map;
+
+public record RecordReportTeamDto(
+        List<Map<String, RecordReportTeamParticipantScoreDto>> top
+        ,List<Map<String, RecordReportTeamParticipantScoreDto>> jungle
+        ,List<Map<String, RecordReportTeamParticipantScoreDto>> middle
+        ,List<Map<String, RecordReportTeamParticipantScoreDto>> bottom
+        ,List<Map<String, RecordReportTeamParticipantScoreDto>> utility
+) {
 }
