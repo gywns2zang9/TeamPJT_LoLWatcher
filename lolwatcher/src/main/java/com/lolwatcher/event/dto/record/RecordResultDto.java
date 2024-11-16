27327a875c6d5d9e2d1ff4d26f3ba1a335704925
Lee Chang Hyun <changhyunlee96@gmail.com>
2024-11-16 21:20:57 +0900

    fix: dto -> report

--- lolwatcher/src/main/java/com/lolwatcher/event/dto/record/RecordResultDto.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/dto/record/RecordResultDto.java
@@ -2,5 +2,5 @@ package com.lolwatcher.event.dto.record;
 
 import java.util.Map;
 
-public record RecordResultDto(RecordMatchDto match, Map<String, Object> reports) {
+public record RecordResultDto(RecordMatchDto match, Map<String, Object> report) {
 }
