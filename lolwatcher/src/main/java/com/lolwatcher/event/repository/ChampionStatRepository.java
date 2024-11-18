67a01a75a0b5c2826ee93b51b4f16da7b8d5330f
이창현 <changhyunlee96@gmail.com>
2024-11-18 15:27:29 +0900

    fix: object return

--- lolwatcher/src/main/java/com/lolwatcher/event/repository/ChampionStatRepository.java
+++ lolwatcher/src/main/java/com/lolwatcher/event/repository/ChampionStatRepository.java
@@ -1,21 +1,23 @@
 package com.lolwatcher.event.repository;
 
-import com.lolwatcher.event.document.ChampionStatAnalysis;
 import com.lolwatcher.event.dto.ChampionStatistics;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ChampionStatRepository {
 
     private final MongoTemplate mongoTemplate;
@@ -23,9 +25,19 @@ public class ChampionStatRepository {
     public List<ChampionStatistics> getChampionStatisticsByObjectId(String collectionName) {
         String objectId = "673ab72aa8d7df664d13b49d";
         Query query = new Query(Criteria.where("_id").is(new ObjectId(objectId)));
-        ChampionStatAnalysis championStatistics = mongoTemplate.findOne(query, ChampionStatAnalysis.class, "analytics");
+        Document document = mongoTemplate.findOne(query, Document.class, "analytics");
+        if(document == null) {
+            log.info("champion statistics not found");
+        }
+        List<Document> statsDocuments = (List<Document>) document.get(collectionName);
+        if (statsDocuments == null) {
+            log.info("Field '{}' not found in document with ObjectId: {}", collectionName, objectId);
+            return null;
+        }
 
-        return Objects.requireNonNull(championStatistics).getData().get(collectionName);
+        return statsDocuments.stream()
+                .map(statDoc -> mongoTemplate.getConverter().read(ChampionStatistics.class, statDoc))
+                .toList();
     }
 
 
