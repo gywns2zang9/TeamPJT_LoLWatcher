153701908164df5555224da2bb52bd55d9ee5bde
gywns2zang9 <gywns2zang9@gmai.com>
2024-11-18 16:03:38 +0900

    fix: report 페이지 삭제

--- lolwatcher/src/App.tsx
+++ lolwatcher/src/App.tsx
@@ -5,7 +5,6 @@ import Main from "./pages/Main";
 import Logout from "./pages/Logout";
 import Champions from "./pages/Champions";
 import Users from "./pages/Users";
-import ResultReport from "./pages/ResultReport";
 import MyPage from "./pages/MyPage";
 import Statistics from "./pages/Statistics";
 import Privacy from "./pages/riot/Privacy";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/champions" element={<Champions />} />
         <Route path="/users" element={<Users />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/result" element={<ResultReport />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/tos" element={<Tos />} />
